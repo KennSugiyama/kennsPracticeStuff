5ecc2e123ff610a3909586b36e17ab9eaf9e489f
Kenn Sugiyama <email": null>
2017-05-23 12:32:13 -0700

    part one DONE

--- one/employee.js
+++ one/employee.js
@@ -1,1 +1,26 @@
+function Employee(employee) {
+  this.firstName = employee.firstName;
+  this.sales = employee.sales;
+  };
+
+
+  Employee.prototype.totalSales = function(){
+    totalSales = this.sales.reduce(add, 0);
+    function add(a, b){
+      return a + b;
+    };
+    return totalSales;
+  };
+
+
+// TRY TO REFACTOR THIS.
+  Employee.prototype.averageSales = function(){
+    totalSales = this.sales.reduce(add, 0);
+    function add(a, b){
+      return a + b;
+    };
+    return totalSales / this.sales.length
+  };
+
+
 
--- one/salesTeam.js
+++ one/salesTeam.js
@@ -1,1 +1,39 @@
+function SalesTeam(employees){
+  this.employees = employees
+};
+
+
+
+// var sidekicks = [
+//     { name: "Robin",     hero: "Batman"   },
+//     { name: "Supergirl", hero: "Superman" },
+//     { name: "Oracle",    hero: "Batman"   },
+//     { name: "Krypto",    hero: "Superman" }
+// ];
+
+// var batKicks = sidekicks.filter(function (el) {
+//     return (el.hero === "Batman");
+// });
+
+
+
+SalesTeam.prototype.find = function(name){
+  var employee = this.employees.filter(function (el){
+    return (el.firstName === name);
+  });
+  return employee[0]
+};
+
+
+
+
+SalesTeam.prototype.topPerformer = function(){
+  var orderedEmployees = this.employees.sort(function(small, large){
+    return large.totalSales() - small.totalSales()
+  })
+  return orderedEmployees[0]
+}
+
+
+
 
